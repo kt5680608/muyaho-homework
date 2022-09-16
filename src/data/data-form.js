d2718df19e9e6ae751f1a1216c9282ebf55c3808
hoon <hoon@hoonui-MacBookPro.local>
2022-09-16 21:15:14 +0900

    feat: data-form 추가

---
+++ src/data/data-form.js
@@ -0,0 +1,16 @@
+export const data = {
+  name: "",
+  level: 0,
+  work: [
+    { name: "카오스던전", doWork: false },
+    { name: "가디언토벌", doWork: false },
+    { name: "에포나", doWork: false },
+    { name: "발탄", doWork: false },
+    { name: "비아키스", doWork: false },
+    { name: "쿠크세이튼", doWork: false },
+    { name: "아브렐슈드", doWork: false },
+    { name: "카양겔", doWork: false },
+    { name: "아그로스", doWork: false },
+    { name: "6종 카드작", doWork: false },
+  ],
+};
